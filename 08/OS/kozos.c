da1bb2575967f13e3220fd16d755d8db2d887dec
RADIO2001 <densikousakusyosinsya@gmail.com>
2017-09-23 13:18:37 +0900

    スレッドが正常終了できない問題を解決

--- 08/OS/kozos.c
+++ 08/OS/kozos.c
@@ -5,57 +5,54 @@
 #include "syscall.h"
 #include "lib.h"
 
-  static void thread_intr(softvec_type_t type, unsigned long sp);
-
 #define THREAD_NUM 6
 #define THREAD_NAME_SIZE 15
 
-/* �����åɡ������ƥ����� */
+/* スレッド・コンテキスト */
 typedef struct _kz_context {
-  uint32 sp; /* �����å����ݥ����� */
+  uint32 sp; /* スタック・ポインタ */
 } kz_context;
 
-/* �������������ȥ����롦�֥��å�(TCB) */
+/* タスク・コントロール・ブロック(TCB) */
 typedef struct _kz_thread {
   struct _kz_thread *next;
-  char name[THREAD_NAME_SIZE + 1]; /* �����å�̾ */
-  char *stack;    /* �����å� */
+  char name[THREAD_NAME_SIZE + 1]; /* スレッド名 */
+  char *stack;    /* スタック */
 
-  struct { /* �����åɤΥ������ȡ����å�(thread_init())���Ϥ��ѥ��᡼�� */
-    kz_func_t func; /* �����åɤΥᥤ���ؿ� */
-    int argc;       /* �����åɤΥᥤ���ؿ����Ϥ� argc */
-    char **argv;    /* �����åɤΥᥤ���ؿ����Ϥ� argv */
+  struct { /* スレッドのスタート・アップ(thread_init())に渡すパラメータ */
+    kz_func_t func; /* スレッドのメイン関数 */
+    int argc;       /* スレッドのメイン関数に渡す argc */
+    char **argv;    /* スレッドのメイン関数に渡す argv */
   } init;
 
-  struct { /* �����ƥࡦ�������ѥХåե� */
+  struct { /* システム・コール用バッファ */
     kz_syscall_type_t type;
     kz_syscall_param_t *param;
   } syscall;
 
-  kz_context context; /* �����ƥ����Ⱦ��� */
+  kz_context context; /* コンテキスト情報 */
 } kz_thread;
 
-/* �����åɤΥ��ǥ��������塼 */
+/* スレッドのレディー・キュー */
 static struct {
   kz_thread *head;
   kz_thread *tail;
 } readyque;
 
-static kz_thread *current; /* �������ȡ������å� */
-static kz_thread threads[THREAD_NUM]; /* �������������ȥ����롦�֥��å� */
-static kz_handler_t handlers[SOFTVEC_TYPE_NUM]; /* �����ߥϥ��ɥ� */
+static kz_thread *current; /* カレント・スレッド */
+static kz_thread threads[THREAD_NUM]; /* タスク・コントロール・ブロック */
+static kz_handler_t handlers[SOFTVEC_TYPE_NUM]; /* 割込みハンドラ */
 
-extern void dispatch1(kz_context *context);
-extern void dispatch2(kz_context *context);
+void dispatch(kz_context *context);
 
-/* �������ȡ������åɤ����ǥ��������塼����ȴ���Ф� */
+/* カレント・スレッドをレディー・キューから抜き出す */
 static int getcurrent(void)
 {
   if (current == NULL) {
     return -1;
   }
 
-  /* �������ȡ������åɤ�ɬ����Ƭ�ˤ����Ϥ��ʤΤǡ���Ƭ����ȴ���Ф� */
+  /* カレント・スレッドは必ず先頭にあるはずなので，先頭から抜き出す */
   readyque.head = current->next;
   if (readyque.head == NULL) {
     readyque.tail = NULL;
@@ -65,14 +62,14 @@ static int getcurrent(void)
   return 0;
 }
 
-/* �������ȡ������åɤ����ǥ��������塼�˷Ҥ��� */
+/* カレント・スレッドをレディー・キューに繋げる */
 static int putcurrent(void)
 {
   if (current == NULL) {
     return -1;
   }
 
-  /* ���ǥ��������塼����������³���� */
+  /* レディー・キューの末尾に接続する */
   if (readyque.tail) {
     readyque.tail->next = current;
   } else {
@@ -88,121 +85,115 @@ static void thread_end(void)
   kz_exit();
 }
 
-/* �����åɤΥ������ȡ����å� */
+/* スレッドのスタート・アップ */
 static void thread_init(kz_thread *thp)
 {
-  /* �����åɤΥᥤ���ؿ����ƤӽФ� */
+  /* スレッドのメイン関数を呼び出す */
   thp->init.func(thp->init.argc, thp->init.argv);
   thread_end();
 }
 
-void start_dispatch(void){
-  puts("diapatch_start");
-}
-
-/* �����ƥࡦ�������ν���(kz_run():�����åɤε�ư) */
+/* システム・コールの処理(kz_run():スレッドの起動) */
 static kz_thread_id_t thread_run(kz_func_t func, char *name,
 				 int stacksize, int argc, char *argv[])
 {
   int i;
   kz_thread *thp;
   uint32 *sp;
-  extern char _userstack; /* ���󥫡��������ץȤ����������륹���å��ΰ� */
-  static char *thread_stack = 0xA0007000;//&_userstack;
+  extern char _userstack; /* リンカ・スクリプトで定義されるスタック領域 */
+  static char *thread_stack = &_userstack;
 
-  /* �����Ƥ��륿�����������ȥ����롦�֥��å��򸡺� */
+  /* 空いているタスク・コントロール・ブロックを検索 */
   for (i = 0; i < THREAD_NUM; i++) {
     thp = &threads[i];
-    if (!thp->init.func) /* ���Ĥ��ä� */
+    if (!thp->init.func) /* 見つかった */
       break;
   }
-  if (i == THREAD_NUM) /* ���Ĥ����ʤ��ä� */
+  if (i == THREAD_NUM) /* 見つからなかった */
     return -1;
 
   memset(thp, 0, sizeof(*thp));
 
-  /* �������������ȥ����롦�֥��å�(TCB)������ */
+  /* タスク・コントロール・ブロック(TCB)の設定 */
   strcpy(thp->name, name);
   thp->next     = NULL;
 
   thp->init.func = func;
   thp->init.argc = argc;
   thp->init.argv = argv;
-  /* �����å��ΰ������� */
+
+  /* スタック領域を獲得 */
   memset(thread_stack, 0, stacksize);
   thread_stack += stacksize;
 
-  thp->stack = thread_stack; /* �����å������� */
+  thp->stack = thread_stack; /* スタックを設定 */
 
-  /* �����å��ν����� */
+  /* スタックの初期化 */
   sp = (uint32 *)thp->stack;
 //  *(--sp) = (uint32)thread_end;
 
   /*
-   * �ץ������ࡦ�����󥿤����ꤹ�롥
+   * プログラム・カウンタを設定する．
    */
-   *(--sp) = 0; /* C0_status */
-   *(--sp) = (uint32)thread_init;/*C0_EPC $12 for eret*/
-putxval(sp,0);
-puts(" ");
-   *(--sp) = 0; /* Hi */
-   *(--sp) = 0; /* Lo */
+   thp->stack = thread_stack; /* �����å������� */
 
-   *(--sp) = (uint32)thread_end; /* $31 */
-   *(--sp) = 0; /* $30 */
- //skip $29
-   *(--sp) = 0; /* $28 */
- //skip $27
- //skip $26
-   *(--sp) = 0; /* $25 */
-   *(--sp) = 0; /* $24 */
-   *(--sp) = 0; /* $23 */
-   *(--sp) = 0; /* $22 */
-   *(--sp) = 0; /* $21 */
-   *(--sp) = 0; /* $20 */
-   *(--sp) = 0; /* $19 */
-   *(--sp) = 0; /* $18 */
-   *(--sp) = 0; /* $17 */
-   *(--sp) = 0; /* $16 */
-   *(--sp) = 0; /* $15 */
-   *(--sp) = 0; /* $14 */
-   *(--sp) = 0; /* $13 */
-   *(--sp) = 0; /* $12 */
-   *(--sp) = 0; /* $11 */
-   *(--sp) = 0; /* $10 */
-   *(--sp) = 0; /* $9 */
-   *(--sp) = 0; /* $8 */
-   *(--sp) = 0; /* $7 */
-   *(--sp) = 0; /* $6 */
-   *(--sp) = 0; /* $5 */
-   *(--sp) = (uint32)thp;  /* $4 $a0 */
-   *(--sp) = 0; /* $3 */
-   *(--sp) = 0; /* $2 */
-   *(--sp) = 0; /* $1 */
-   putxval(sp,0);
-   puts(" ");
-  /* �����åɤΥ������ȡ����å�(thread_init())���Ϥ����� */
-//  *(--sp) = (uint32)thp;  /* ER0 */
-  /* �����åɤΥ����ƥ����Ȥ����� */
+    *(--sp) = 0; /* C0_status */
+    *(--sp) = (uint32)thread_init;/*C0_EPC $12 for eret*/
+
+    *(--sp) = 0; /* Hi */
+    *(--sp) = 0; /* Lo */
+
+    *(--sp) = (uint32)thread_end; /* $31 */
+    *(--sp) = 0; /* $30 */
+    *(--sp) = 0;//skip $29
+    *(--sp) = 0; /* $28 */
+    *(--sp) = 0;//skip $27
+    *(--sp) = 0;//skip $26
+    *(--sp) = 0; /* $25 */
+    *(--sp) = 0; /* $24 */
+    *(--sp) = 0; /* $23 */
+    *(--sp) = 0; /* $22 */
+    *(--sp) = 0; /* $21 */
+    *(--sp) = 0; /* $20 */
+    *(--sp) = 0; /* $19 */
+    *(--sp) = 0; /* $18 */
+    *(--sp) = 0; /* $17 */
+    *(--sp) = 0; /* $16 */
+    *(--sp) = 0; /* $15 */
+    *(--sp) = 0; /* $14 */
+    *(--sp) = 0; /* $13 */
+    *(--sp) = 0; /* $12 */
+    *(--sp) = 0; /* $11 */
+    *(--sp) = 0; /* $10 */
+    *(--sp) = 0; /* $9 */
+    *(--sp) = 0; /* $8 */
+    *(--sp) = 0; /* $7 */
+    *(--sp) = 0; /* $6 */
+    *(--sp) = 0; /* $5 */
+    *(--sp) = (uint32)thp;  /* $4 $a0 */
+    *(--sp) = 0; /* $3 */
+    *(--sp) = 0; /* $2 */
+    *(--sp) = 0; /* $1 */
+
+
+  /* スレッドのコンテキストを設定 */
   thp->context.sp = (uint32)sp;
 
-  /* �����ƥࡦ���������ƤӽФ��������åɤ����ǥ��������塼���᤹ */
+  /* システム・コールを呼び出したスレッドをレディー・キューに戻す */
   putcurrent();
 
-  /* �����������������åɤ򡤥��ǥ��������塼����³���� */
+  /* 新規作成したスレッドを，レディー・キューに接続する */
   current = thp;
   putcurrent();
-  putxval(thp->context.sp,0);
-  puts(" ");
   return (kz_thread_id_t)current;
 }
 
-/* �����ƥࡦ�������ν���(kz_exit():�����åɤν�λ) */
+/* システム・コールの処理(kz_exit():スレッドの終了) */
 static int thread_exit(void)
 {
   /*
-   * �����ʤ饹���å����������ƺ����ѤǤ����褦�ˤ��٤�������ά��
-   * ���Τ��ᡤ�����åɤ����ˤ��������õ���褦�ʤ��Ȥϸ����ǤǤ��ʤ���
+   * 本来ならスタックも解放して再利用できるようにすべきだが省略．
+   * このため，スレッドを頻繁に生成・消去するようなことは現状でできない．
    */
   puts(current->name);
   puts(" EXIT.\n");
@@ -210,25 +201,27 @@ static int thread_exit(void)
   return 0;
 }
 
-/* �����ߥϥ��ɥ�����Ͽ */
+  static void thread_intr(softvec_type_t type, unsigned long sp);
+
+/* 割込みハンドラの登録 */
 static int setintr(softvec_type_t type, kz_handler_t handler)
 {
 
 
   /*
-   * �����ߤ������դ��뤿���ˡ����եȥ������������ߥ٥�����
-   * OS�γ����߽����������Ȥʤ��ؿ�����Ͽ���롥
+   * 割込みを受け付けるために，ソフトウエア・割込みベクタに
+   * OSの割込み処理の入口となる関数を登録する．
    */
   softvec_setintr(type, thread_intr);
 
-  handlers[type] = handler; /* OS¦�����ƤӽФ������ߥϥ��ɥ�����Ͽ */
+  handlers[type] = handler; /* OS側から呼び出す割込みハンドラを登録 */
 
   return 0;
 }
 
 static void call_functions(kz_syscall_type_t type, kz_syscall_param_t *p)
 {
-  /* �����ƥࡦ�������μ¹�����current���񤭴������Τ����� */
+  /* システム・コールの実行中にcurrentが書き換わるので注意 */
   switch (type) {
   case KZ_SYSCALL_TYPE_RUN: /* kz_run() */
     p->un.run.ret = thread_run(p->un.run.func, p->un.run.name,
@@ -236,7 +229,7 @@ static void call_functions(kz_syscall_type_t type, kz_syscall_param_t *p)
 			       p->un.run.argc, p->un.run.argv);
     break;
   case KZ_SYSCALL_TYPE_EXIT: /* kz_exit() */
-    /* TCB���õ�����Τǡ������ͤ��񤭹����ǤϤ����ʤ� */
+    /* TCBが消去されるので，戻り値を書き込んではいけない */
     thread_exit();
     break;
   default:
@@ -244,26 +237,26 @@ static void call_functions(kz_syscall_type_t type, kz_syscall_param_t *p)
   }
 }
 
-/* �����ƥࡦ�������ν��� */
+/* システム・コールの処理 */
 static void syscall_proc(kz_syscall_type_t type, kz_syscall_param_t *p)
 {
   /*
-   * �����ƥࡦ���������ƤӽФ��������åɤ����ǥ��������塼����
-   * ���������֤ǽ����ؿ����ƤӽФ������Τ��᥷���ƥࡦ��������
-   * �ƤӽФ��������åɤ򤽤Τޤ�ư����³�������������ˤϡ�
-   * �����ؿ��������� putcurrent() ���Ԥ�ɬ�פ����롥
+   * システム・コールを呼び出したスレッドをレディー・キューから
+   * 外した状態で処理関数を呼び出す．このためシステム・コールを
+   * 呼び出したスレッドをそのまま動作継続させたい場合には，
+   * 処理関数の内部で putcurrent() を行う必要がある．
    */
   getcurrent();
   call_functions(type, p);
 }
 
-/* �����åɤΥ������塼������ */
+/* スレッドのスケジューリング */
 static void schedule(void)
 {
-  if (!readyque.head) /* ���Ĥ����ʤ��ä� */
+  if (!readyque.head) /* 見つからなかった */
     kz_sysdown();
 
-  current = readyque.head; /* �������ȡ������åɤ����ꤹ�� */
+  current = readyque.head; /* カレント・スレッドに設定する */
 }
 
 static void syscall_intr(void)
@@ -275,42 +268,42 @@ static void softerr_intr(void)
 {
   puts(current->name);
   puts(" DOWN.\n");
-  getcurrent(); /* ���ǥ������塼���鳰�� */
-  thread_exit(); /* �����åɽ�λ���� */
+  getcurrent(); /* レディーキューから外す */
+  thread_exit(); /* スレッド終了する */
 }
 
-/* �����߽����������ؿ� */
+/* 割込み処理の入口関数 */
 static void thread_intr(softvec_type_t type, unsigned long sp)
 {
-  /* �������ȡ������åɤΥ����ƥ����Ȥ���¸���� */
+  /* カレント・スレッドのコンテキストを保存する */
   current->context.sp = sp;
 
   /*
-   * �����ߤ��Ȥν������¹Ԥ��롥
-   * SOFTVEC_TYPE_SYSCALL, SOFTVEC_TYPE_SOFTERR �ξ�����
-   * syscall_intr(), softerr_intr() ���ϥ��ɥ�����Ͽ�����Ƥ����Τǡ�
-   * �����餬�¹Ԥ����롥
+   * 割込みごとの処理を実行する．
+   * SOFTVEC_TYPE_SYSCALL, SOFTVEC_TYPE_SOFTERR の場合は
+   * syscall_intr(), softerr_intr() がハンドラに登録されているので，
+   * それらが実行される．
    */
   if (handlers[type])
     handlers[type]();
 
-  schedule(); /* �����åɤΥ������塼������ */
+  schedule(); /* スレッドのスケジューリング */
 
   /*
-   * �����åɤΥǥ����ѥå�
-   * (dispatch()�ؿ������Τ�startup.s�ˤ��ꡤ�������֥��ǵ��Ҥ����Ƥ���)
+   * スレッドのディスパッチ
+   * (dispatch()関数の本体はstartup.sにあり，アセンブラで記述されている)
    */
-   current->context.sp-=8;
-  dispatch2(0xa0007280);
-  /* �����ˤ��֤äƤ��ʤ� */
+
+  dispatch(current->context.sp);
+  /* ここには返ってこない */
 }
 
 void kz_start(kz_func_t func, char *name, int stacksize,
 	      int argc, char *argv[])
 {
   /*
-   * �ʹߤǸƤӽФ������åɴ�Ϣ�Υ饤�֥����ؿ��������� current ��
-   * ���Ƥ������礬�����Τǡ�current �� NULL �˽��������Ƥ�����
+   * 以降で呼び出すスレッド関連のライブラリ関数の内部で current を
+   * 見ている場合があるので，current を NULL に初期化しておく．
    */
   current = NULL;
 
@@ -318,18 +311,17 @@ void kz_start(kz_func_t func, char *name, int stacksize,
   memset(threads,  0, sizeof(threads));
   memset(handlers, 0, sizeof(handlers));
 
-  /* �����ߥϥ��ɥ�����Ͽ */
-  setintr(SOFTVEC_TYPE_SYSCALL, syscall_intr); /* �����ƥࡦ������ */
-  setintr(SOFTVEC_TYPE_SOFTERR, softerr_intr); /* �������װ�ȯ�� */
+  /* 割込みハンドラの登録 */
+  setintr(SOFTVEC_TYPE_SYSCALL, syscall_intr); /* システム・コール */
+  setintr(SOFTVEC_TYPE_SOFTERR, softerr_intr); /* ダウン要因発生 */
 
-  /* �����ƥࡦ������ȯ���ԲĤʤΤ�ľ�ܴؿ����ƤӽФ��ƥ����åɺ������� */
+  /* システム・コール発行不可なので直接関数を呼び出してスレッド作成する */
   current = (kz_thread *)thread_run(func, name, stacksize, argc, argv);
-  /* �ǽ��Υ����åɤ���ư */
 
-  putxval(current->context.sp,0);
-  puts(" ");
-  dispatch1(current->context.sp);
-  /* �����ˤ��֤äƤ��ʤ� */
+  /* 最初のスレッドを起動 */
+  dispatch(current->context.sp);
+
+  /* ここには返ってこない */
 }
 
 void kz_sysdown(void)
@@ -339,10 +331,10 @@ void kz_sysdown(void)
     ;
 }
 
-/* �����ƥࡦ�������ƤӽФ��ѥ饤�֥����ؿ� */
+/* システム・コール呼び出し用ライブラリ関数 */
 void kz_syscall(kz_syscall_type_t type, kz_syscall_param_t *param)
 {
   current->syscall.type  = type;
   current->syscall.param = param;
-  asm volatile ("syscall"); /* �ȥ��å׳�����ȯ�� */
+  asm volatile ("syscall"); /* トラップ割込み発行 */
 }
