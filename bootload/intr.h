5d716f381ade1432622841de4d615dcb9edf8c65
envzhu <densikousakusyosinsya@gmail.com>
2018-02-01 12:53:19 +0900

    arrange interrupt handler code

--- bootload/intr.h
+++ bootload/intr.h
@@ -3,10 +3,12 @@
 
 /* ソフトウエア・割込みベクタの定義 */
 
-#define SOFTVEC_TYPE_NUM     3
+#define SOFTVEC_TYPE_NUM     5
 
 #define SOFTVEC_TYPE_SOFTERR 0
 #define SOFTVEC_TYPE_SYSCALL 1
 #define SOFTVEC_TYPE_SERINTR 2
+#define SOFTVEC_TYPE_USBINTR 3
+#define SOFTVEC_TYPE_TMRINTR 4
 
 #endif
