4da36f1591cdd5cd98abd43a9862296f66ff1937
envzhu <densikousakusyosinsya@gmail.com>
2018-02-02 20:35:49 +0900

    複数のタイマを同時に使えるよう試行錯誤中

--- os/tmrdrv.c
+++ os/tmrdrv.c
@@ -13,9 +13,12 @@ typedef struct _tmrbuf {
 	int msec;
 }tmrbuf;
 
+static struct tmrreg {
+	tmrbuf *head;
+	tmrbuf *tail;
+} tmrreg[TMRDRV_DEVICE_NUM];
 
-tmrbuf *head;
-tmrbuf *tail;
+
 
 void tmr_stop(tmrbuf *p_timerbuf);
 
@@ -29,28 +32,40 @@ void tmr_stop(tmrbuf *p_timerbuf);
  * また非コンテキスト状態で呼ばれるため，システム・コールは利用してはいけない．
  * (サービス・コールを利用すること)
  */
-
-
-/* 割込みハンドラ */
-static void tmrdrv_intr(void)
-{
+static inline void tmrdrv_intrproc(struct tmrreg *tmr){
 	tmrbuf *p_timerbuf;
-	p_timerbuf = head;
+	p_timerbuf = tmr->head;
 	timer_expire(p_timerbuf->index);
 	do{
 		p_timerbuf->msec--;
 		if(p_timerbuf->msec==0){
+			puts("tmrdrv_intrproc\n");
 			tmr_stop(p_timerbuf);
 			kx_send(MSGBOX_ID_TMRINPUT, 0, NULL);
 		}
 	}while((p_timerbuf->next == NULL) && (p_timerbuf = p_timerbuf->next));
 }
 
+
+/* 割込みハンドラ */
+static void tmrdrv_intr(void)
+{
+  int i;
+  struct tmrreg *tmr;
+
+  for (i = 0; i < TMRDRV_DEVICE_NUM; i++) {
+    tmr = &tmrreg[i];
+    if (tmr->head) {
+      if (timer_is_expired(i))
+	/* 割込みがあるならば，割込み処理を呼び出す */
+	tmrdrv_intrproc(tmr);
+    }
+  }
+}
+
 static int tmrdrv_init(void)
 {
-  //memset(tmrdev, 0, sizeof(tmrdev));
-	head=NULL;
-	tail=NULL;
+  memset(tmrreg, 0, sizeof(tmrreg));
   return 0;
 }
 
@@ -60,30 +75,30 @@ static void tmr_start(int index, int msec)
 	tmrbuf *p_tmrbuf = kz_kmalloc(sizeof(tmrbuf));
 	p_tmrbuf->index = index;
 	p_tmrbuf->msec = 1000;
-	if(tail){
-		tail->next = p_tmrbuf;
-		p_tmrbuf->next = tail;
-	}else{
-		head = p_tmrbuf;
+	if(tmrreg[index].tail){
+		tmrreg[index].tail->next = p_tmrbuf;
+		p_tmrbuf->prev = tmrreg[index].tail;
+		}else{
+		timer_start(index);
+		tmrreg[index].head = p_tmrbuf;
 	}
-	tail = p_tmrbuf;
-	timer_start(index);
+	tmrreg[index].tail = p_tmrbuf;
 }
 
 void tmr_stop(tmrbuf *p_timerbuf)
 {
-	if(head == p_timerbuf){
+	if(tmrreg[p_timerbuf->index].head == p_timerbuf){
 		if(p_timerbuf->next == NULL){
 			timer_stop(p_timerbuf->index);
-			head = NULL;
-			tail = NULL;
+			tmrreg[p_timerbuf->index].head = NULL;
+			tmrreg[p_timerbuf->index].tail = NULL;
 		}else{
-			head = p_timerbuf->next;
-			head->prev = NULL;
+			tmrreg[p_timerbuf->index].head = p_timerbuf->next;
+			tmrreg[p_timerbuf->index].head->prev = NULL;
 		}
-	}else if(tail==p_timerbuf){
-		tail = p_timerbuf->prev;
-		tail->next=NULL;
+	}else if(tmrreg[p_timerbuf->index].tail == p_timerbuf){
+		tmrreg[p_timerbuf->index].tail = p_timerbuf->prev;
+		tmrreg[p_timerbuf->index].tail->next=NULL;
 	}else{
 		p_timerbuf->prev->next = p_timerbuf->next;
 		p_timerbuf->next->prev = p_timerbuf->prev;
@@ -125,4 +140,19 @@ int tmrdrv_main(int argc, char *argv[])
   }
 
   return 0;
+}
+
+
+void tmr_sleep(int msec){
+  char *p;
+  p = kz_kmalloc(6);
+  p[0] = '0' + TMR_DEFAULT_DEVICE;
+  p[1] = TMRDRV_CMD_START;
+  p[2] = msec>>24;
+  p[3] = (msec>>16)&0xFF;
+  p[4] = (msec>>8)&0xFF;
+  p[5] = msec&0xFF;
+
+  kz_send(MSGBOX_ID_TMROUTPUT, 6, p);
+  kz_recv(MSGBOX_ID_TMRINPUT, NULL, NULL);
 }
