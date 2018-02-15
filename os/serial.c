91696e0bf19dd6c48ff1191a7616449dc76ea4fb
envzhu <densikousakusyosinsya@gmail.com>
2018-02-15 19:39:56 +0900

    fix serial.c

--- os/serial.c
+++ os/serial.c
@@ -1,13 +1,13 @@
 #include "defines.h"
 #include "serial.h"
 
-#define SERIAL_SCI_NUM 2
+#define SERIAL_UART_NUM 2
 
-#define PIC_SCI1 ((volatile struct pic_sci *)0xBF806100)
-#define PIC_SCI2 ((volatile struct pic_sci *)0xBF806200)
+#define PIC_UART1 ((volatile struct pic_uart *)0xBF806100)
+#define PIC_UART2 ((volatile struct pic_uart *)0xBF806200)
 
 
-struct pic_sci {
+struct pic_uart {
   volatile uint32 UxMODE;
   volatile uint32 UxMODECLR;
   volatile uint32 UxMODESET;
@@ -43,106 +43,122 @@ struct pic_sci {
 #define IFS1CLR *((volatile unsigned int *)0xBF881044)
 #define IFS1SET *((volatile unsigned int *)0xBF881048)
 
+#define IPC8    *((volatile unsigned int *)0xBF881110)
+#define IPC8CLR *((volatile unsigned int *)0xBF881114)
+#define IPC8SET *((volatile unsigned int *)0xBF881118)
+#define IPC9    *((volatile unsigned int *)0xBF881120)
+#define IPC9CLR *((volatile unsigned int *)0xBF881124)
+#define IPC9SET *((volatile unsigned int *)0xBF881128)
+
 //UxMODE register
-#define PIC_SCI_UxMODE_STSEL  (1<<0)
-#define PIC_SCI_UxMODE_PDSEL_8bit_none_parity (0<<1)
-#define PIC_SCI_UxMODE_PDSEL_8bit_odd_parity  (1<<1)
-#define PIC_SCI_UxMODE_PDSEL_8bit_even_parity (2<<1)
-#define PIC_SCI_UxMODE_PDSEL_9bit_none_parity (3<<1)
-#define PIC_SCI_UxMODE_BRGH   (1<<3)
-#define PIC_SCI_UxMODE_RXINV  (1<<4)
-#define PIC_SCI_UxMODE_ABAUD  (1<<5)
-#define PIC_SCI_UxMODE_LPBACK (1<<6)
-#define PIC_SCI_UxMODE_WAKE   (1<<7)
-#define PIC_SCI_UxMODE_UEN_USE_UxTX_UxRX              (0<<8)
-#define PIC_SCI_UxMODE_UEN_USE_UxTX_UxRX_UxCTS        (1<<8)
-#define PIC_SCI_UxMODE_UEN_USE_UxTX_UxRX_UxCTS_UxRTS  (2<<8)
-#define PIC_SCI_UxMODE_UEN_USE_UxTX_UxRX_UxBCLK       (3<<8)
+#define PIC_UART_UxMODE_STSEL  (1<<0)
+#define PIC_UART_UxMODE_PDSEL_8bit_none_parity (0<<1)
+#define PIC_UART_UxMODE_PDSEL_8bit_odd_parity  (1<<1)
+#define PIC_UART_UxMODE_PDSEL_8bit_even_parity (2<<1)
+#define PIC_UART_UxMODE_PDSEL_9bit_none_parity (3<<1)
+#define PIC_UART_UxMODE_BRGH   (1<<3)
+#define PIC_UART_UxMODE_RXINV  (1<<4)
+#define PIC_UART_UxMODE_ABAUD  (1<<5)
+#define PIC_UART_UxMODE_LPBACK (1<<6)
+#define PIC_UART_UxMODE_WAKE   (1<<7)
+#define PIC_UART_UxMODE_UEN_USE_UxTX_UxRX              (0<<8)
+#define PIC_UART_UxMODE_UEN_USE_UxTX_UxRX_UxCTS        (1<<8)
+#define PIC_UART_UxMODE_UEN_USE_UxTX_UxRX_UxCTS_UxRTS  (2<<8)
+#define PIC_UART_UxMODE_UEN_USE_UxTX_UxRX_UxBCLK       (3<<8)
 //skip 10bit  read as 0
-#define PIC_SCI_UxMODE_RTSMD  (1<<11)
-#define PIC_SCI_UxMODE_IREN   (1<<12)
-#define PIC_SCI_UxMODE_SIDL   (1<<13)
+#define PIC_UART_UxMODE_RTSMD  (1<<11)
+#define PIC_UART_UxMODE_IREN   (1<<12)
+#define PIC_UART_UxMODE_SIDL   (1<<13)
 //skip 14bit  read as 0
-#define PIC_SCI_UxMODE_ON     (1<<15)
+#define PIC_UART_UxMODE_ON     (1<<15)
 
 //UxSTA register
-#define PIC_SCI_UxSTA_URXDA   (1<<0)
-#define PIC_SCI_UxSTA_OERR    (1<<1)
-#define PIC_SCI_UxSTA_FERR    (1<<2)
-#define PIC_SCI_UxSTA_PERR    (1<<3)
-#define PIC_SCI_UxSTA_RIDLE   (1<<4)
-#define PIC_SCI_UxSTA_ADDEN   (1<<5)
-#define PIC_SCI_UxSTA_URXTISEL_EVERY  (0<<6)
-#define PIC_SCI_UxSTA_URXTISEL_THREE  (2<<6)
-#define PIC_SCI_UxSTA_URXTISEL_FULL   (3<<6)
-#define PIC_SCI_UxSTA_TRMT    (1<<8)
-#define PIC_SCI_UxSTA_UTXBF   (1<<9)
-#define PIC_SCI_UxSTA_UTXEN   (1<<10)
-#define PIC_SCI_UxSTA_UTXBRK  (1<<11)
-#define PIC_SCI_UxSTA_URXEN   (1<<12)
-#define PIC_SCI_UxSTA_UTXINV  (1<<13)
-#define PIC_SCI_UxSTA_UTXISEL_BUFFER_SOME_FREE  (0<<14)
-#define PIC_SCI_UxSTA_UTXISEL_EVERY_DATA        (1<<14)
-#define PIC_SCI_UxSTA_UTXISEL_BUFFER_ALL_FREE   (2<<14)
-#define PIC_SCI_UxSTA_UTXISEL_DONOT_USE         (3<<14)
+#define PIC_UART_UxSTA_URXDA   (1<<0)
+#define PIC_UART_UxSTA_OERR    (1<<1)
+#define PIC_UART_UxSTA_FERR    (1<<2)
+#define PIC_UART_UxSTA_PERR    (1<<3)
+#define PIC_UART_UxSTA_RIDLE   (1<<4)
+#define PIC_UART_UxSTA_ADDEN   (1<<5)
+#define PIC_UART_UxSTA_URXTISEL_EVERY  (0<<6)
+#define PIC_UART_UxSTA_URXTISEL_THREE  (2<<6)
+#define PIC_UART_UxSTA_URXTISEL_FULL   (3<<6)
+#define PIC_UART_UxSTA_TRMT    (1<<8)
+#define PIC_UART_UxSTA_UTXBF   (1<<9)
+#define PIC_UART_UxSTA_UTXEN   (1<<10)
+#define PIC_UART_UxSTA_UTXBRK  (1<<11)
+#define PIC_UART_UxSTA_URXEN   (1<<12)
+#define PIC_UART_UxSTA_UTXINV  (1<<13)
+#define PIC_UART_UxSTA_UTXISEL_BUFFER_SOME_FREE  (0<<14)
+#define PIC_UART_UxSTA_UTXISEL_EVERY_DATA        (1<<14)
+#define PIC_UART_UxSTA_UTXISEL_BUFFER_ALL_FREE   (2<<14)
+#define PIC_UART_UxSTA_UTXISEL_DONOT_USE         (3<<14)
 // bit 23-16 is read only
-#define PIC_SCI_UxSTA_ADM_EN  (1<<24)
+#define PIC_UART_UxSTA_ADM_EN  (1<<24)
 
 //UART Interrupts Registers
-#define PIC_SCI1_RECV_INTTERUPT_FLAG    (1<<8)
-#define PIC_SCI1_RECV_INTTERUPT_ENABLE  (1<<8)
-#define PIC_SCI1_SEND_INTTERUPT_FLAG    (1<<9)
-#define PIC_SCI1_SEND_INTTERUPT_ENABLE  (1<<9)
+#define PIC_UART1_RECV_INTTERUPT_FLAG    (1<<8)
+#define PIC_UART1_RECV_INTTERUPT_ENABLE  (1<<8)
+#define PIC_UART1_SEND_INTTERUPT_FLAG    (1<<9)
+#define PIC_UART1_SEND_INTTERUPT_ENABLE  (1<<9)
 
-#define PIC_SCI2_RECV_INTTERUPT_FLAG    (1<<22)
-#define PIC_SCI2_RECV_INTTERUPT_ENABLE  (1<<22)
-#define PIC_SCI2_SEND_INTTERUPT_FLAG    (1<<23)
-#define PIC_SCI2_SEND_INTTERUPT_ENABLE  (1<<23)
+#define PIC_UART2_RECV_INTTERUPT_FLAG    (1<<22)
+#define PIC_UART2_RECV_INTTERUPT_ENABLE  (1<<22)
+#define PIC_UART2_SEND_INTTERUPT_FLAG    (1<<23)
+#define PIC_UART2_SEND_INTTERUPT_ENABLE  (1<<23)
 
 
 static struct {
-  volatile struct pic_sci *sci;
-} regs[SERIAL_SCI_NUM] = {
-  { PIC_SCI1 },
-  { PIC_SCI2 },
+  volatile struct pic_uart *uart;
+} regs[SERIAL_UART_NUM] = {
+  { PIC_UART1 },
+  { PIC_UART2 },
 };
 
 /* デバイス初期化 */
 int serial_init(int index)
 {
-  volatile struct pic_sci *sci = regs[index].sci;
+  if(!((index==0)|(index==1)))  return -1; 
 
-  if(index==0)
-    IFS1CLR=PIC_SCI1_RECV_INTTERUPT_FLAG|PIC_SCI1_SEND_INTTERUPT_FLAG;
-  
-  if(index==1)
-    IFS1CLR=PIC_SCI2_RECV_INTTERUPT_FLAG|PIC_SCI2_SEND_INTTERUPT_FLAG;
+  volatile struct pic_uart *uart = regs[index].uart;
 
-  return 0;
+  uart ->UxSTA   = 0x0;
+  uart ->UxTXREG = 0x0;
+  uart ->UxBRG   = F_PBCLK / 9600 / 16 - 1; //UxBRG = F_PBCLK / baud_rate / 16
+  uart ->UxSTASET= PIC_UART_UxSTA_URXEN | PIC_UART_UxSTA_UTXEN;
+  uart ->UxMODE  = PIC_UART_UxMODE_ON;
+
+  if(index==0){
+    IPC8SET = 0b01001;
+    IFS1CLR = PIC_UART1_RECV_INTTERUPT_FLAG|PIC_UART1_SEND_INTTERUPT_FLAG;
+	}else{
+    IPC9SET = (0b00101<<8);
+    IFS1CLR = PIC_UART2_RECV_INTTERUPT_FLAG|PIC_UART2_SEND_INTTERUPT_FLAG;
+	}
+	return 0;
 }
 
 /* 送信可能か？ */
 int serial_is_send_enable(int index)
 {
-  volatile struct pic_sci *sci = regs[index].sci;
-  return (!(sci->UxSTA&PIC_SCI_UxSTA_UTXBF));
+  volatile struct pic_uart *uart = regs[index].uart;
+  return (!(uart->UxSTA&PIC_UART_UxSTA_UTXBF));
 }
 
 
 /* １文字送信 */
 int serial_send_byte(int index, unsigned char c)
 {
-  volatile struct pic_sci *sci = regs[index].sci;
+  volatile struct pic_uart *uart = regs[index].uart;
 
   /* 送信可能になるまで待つ */
   while (!serial_is_send_enable(index));
-  sci ->UxTXREG = c; /* 送信開始 */
+  uart ->UxTXREG = c; /* 送信開始 */
   
   if(index==0)
-    IFS1CLR=PIC_SCI1_SEND_INTTERUPT_FLAG;
+    IFS1CLR=PIC_UART1_SEND_INTTERUPT_FLAG;
   
   if(index==1)
-    IFS1CLR=PIC_SCI2_SEND_INTTERUPT_FLAG;
+    IFS1CLR=PIC_UART2_SEND_INTTERUPT_FLAG;
 
   return 0;
 }
@@ -150,29 +166,29 @@ int serial_send_byte(int index, unsigned char c)
 /* 受信可能か？ */
 int serial_is_recv_enable(int index)
 {
-  volatile struct pic_sci *sci = regs[index].sci;
-  return (sci ->UxSTA & PIC_SCI_UxSTA_URXDA);
+  volatile struct pic_uart *uart = regs[index].uart;
+  return (uart ->UxSTA & PIC_UART_UxSTA_URXDA);
 }
 
 /* １文字受信 */
 unsigned char serial_recv_byte(int index)
 {
-  volatile struct pic_sci *sci = regs[index].sci;
+  volatile struct pic_uart *uart = regs[index].uart;
   unsigned char c;
 
   /*受信文字が来るまで待つ*/
   while(!serial_is_recv_enable(index));
-  c = sci ->UxRXREG;
+  c = uart ->UxRXREG;
 /*
-  if ((sci ->UxSTA == PIC_SCI_UxSTA_OERR))
+  if ((uart ->UxSTA == PIC_UART_UxSTA_OERR))
   {
-      sci ->UxSTACLR = PIC_SCI_UxSTA_OERR;
+      uart ->UxSTACLR = PIC_UART_UxSTA_OERR;
   }*/
   if(index==0)
-    IFS1CLR=PIC_SCI1_RECV_INTTERUPT_FLAG;
+    IFS1CLR=PIC_UART1_RECV_INTTERUPT_FLAG;
   
   if(index==1)
-    IFS1CLR=PIC_SCI2_RECV_INTTERUPT_FLAG;
+    IFS1CLR=PIC_UART2_RECV_INTTERUPT_FLAG;
 
   return c;
 }
@@ -181,10 +197,10 @@ unsigned char serial_recv_byte(int index)
 int serial_intr_is_send_enable(int index)
 {
   if(index==0)
-    return (IEC1 & PIC_SCI1_SEND_INTTERUPT_ENABLE) ? 1 : 0;;
+    return (IEC1 & PIC_UART1_SEND_INTTERUPT_ENABLE) ? 1 : 0;;
   
   if(index==1)
-    return (IEC1 & PIC_SCI2_SEND_INTTERUPT_ENABLE) ? 1 : 0;
+    return (IEC1 & PIC_UART2_SEND_INTTERUPT_ENABLE) ? 1 : 0;
 
   return -1;
 }
@@ -193,10 +209,10 @@ int serial_intr_is_send_enable(int index)
 void serial_intr_send_enable(int index)
 {
   if(index==0)
-    IEC1SET=PIC_SCI1_SEND_INTTERUPT_FLAG;
+    IEC1SET=PIC_UART1_SEND_INTTERUPT_FLAG;
   
   if(index==1)
-    IEC1SET=PIC_SCI2_SEND_INTTERUPT_FLAG;
+    IEC1SET=PIC_UART2_SEND_INTTERUPT_FLAG;
 
 }
 
@@ -204,10 +220,10 @@ void serial_intr_send_enable(int index)
 void serial_intr_send_disable(int index)
 {
   if(index==0)
-    IEC1CLR=PIC_SCI1_SEND_INTTERUPT_FLAG;
+    IEC1CLR=PIC_UART1_SEND_INTTERUPT_FLAG;
   
   if(index==1)
-    IEC1CLR=PIC_SCI2_SEND_INTTERUPT_FLAG;
+    IEC1CLR=PIC_UART2_SEND_INTTERUPT_FLAG;
 
 }
 
@@ -215,10 +231,10 @@ void serial_intr_send_disable(int index)
 int serial_intr_is_recv_enable(int index)
 {
   if(index==0)
-    return (IEC1 & PIC_SCI1_RECV_INTTERUPT_ENABLE) ? 1 : 0;;
+    return (IEC1 & PIC_UART1_RECV_INTTERUPT_ENABLE) ? 1 : 0;;
   
   if(index==1)
-    return (IEC1 & PIC_SCI2_RECV_INTTERUPT_ENABLE) ? 1 : 0;
+    return (IEC1 & PIC_UART2_RECV_INTTERUPT_ENABLE) ? 1 : 0;
 
   return -1;
 }
@@ -227,10 +243,10 @@ int serial_intr_is_recv_enable(int index)
 void serial_intr_recv_enable(int index)
 {
   if(index==0)
-    IEC1SET=PIC_SCI1_RECV_INTTERUPT_FLAG;
+    IEC1SET=PIC_UART1_RECV_INTTERUPT_FLAG;
   
   if(index==1)
-    IEC1SET=PIC_SCI2_RECV_INTTERUPT_FLAG;
+    IEC1SET=PIC_UART2_RECV_INTTERUPT_FLAG;
 
 }
 
@@ -238,9 +254,9 @@ void serial_intr_recv_enable(int index)
 void serial_intr_recv_disable(int index)
 {
   if(index==0)
-    IEC1CLR=PIC_SCI1_RECV_INTTERUPT_FLAG;
+    IEC1CLR=PIC_UART1_RECV_INTTERUPT_FLAG;
   
   if(index==1)
-    IEC1CLR=PIC_SCI2_RECV_INTTERUPT_FLAG;
+    IEC1CLR=PIC_UART2_RECV_INTTERUPT_FLAG;
 
 }
